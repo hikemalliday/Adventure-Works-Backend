6b53c648906b3d3ba6d3e9912d17bdcc2233cb37
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2023-11-26 16:37:35 -0600

    conversion from MSSQL to SQLite

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import jwt
+
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import create_engine, MetaData, Table, text, delete
+from sqlalchemy import create_engine, text
+
 from sqlalchemy.orm import sessionmaker
 from auth import AuthHandler
 from schemas import AuthDetails
+import sqlite3
 
-engine = create_engine("mssql+pyodbc://sa:changeme@DESKTOP-RIAC343\\SQLEXPRESS/AdventureWorks2019?driver=ODBC+Driver+17+for+SQL+Server")
-Session = sessionmaker(bind = engine)
-session = Session()
+engine_mssql = create_engine("mssql+pyodbc://sa:changeme@DESKTOP-RIAC343\\SQLEXPRESS/AdventureWorks2019?driver=ODBC+Driver+17+for+SQL+Server")
+engine_sqlite = create_engine("sqlite:///./.venv/server/master.db")
+
+Session_mssql = sessionmaker(bind = engine_mssql)
+session_mssql = Session_mssql()
+
+Session_sqlite = sessionmaker(bind = engine_sqlite)
+session_sqlite = Session_sqlite()
+
 app = FastAPI()
 auth_handler = AuthHandler()
 
@@ -30,11 +38,16 @@ app.add_middleware(
 
 @app.post("/createusernameandpassword", status_code = 201)
 def register(body: dict):
+     print('test test test')
      # Fetch / GET table 'users', and append the col 'usernames' in the list:
+     # query_string = "SELECT username from [AdventureWorks2019].[Person].[UsernameAndPassword]"
+     # query = text(query_string)
+     # results = session_mssql.execute(query).fetchall()
      users = []
-     query_string = "SELECT username from [AdventureWorks2019].[Person].[UsernameAndPassword]"
-     query = text(query_string)
-     results = session.execute(query).fetchall()
+     conn = sqlite3.connect('./master.db')
+     c = conn.cursor()
+     c.execute("""SELECT * FROM username_and_password""")
+     results = c.fetchall()
      
      for user in results:
           users.append(user[0])
@@ -42,35 +55,46 @@ def register(body: dict):
      if any(x == body['username'] for x in users):
           raise HTTPException(status_code = 400, detail = 'Username is taken')
      
+     # If username does not already exist, create a hashed password
      hashed_password = auth_handler.get_password_hash(body['password'])
-     query_string = f"INSERT INTO [AdventureWorks2019].[Person].[UsernameAndPassword] VALUES ('{body['username']}' , '{hashed_password}', '{body['admin']}')"
-
-     query = text(query_string)
-     session.execute(query)
-     session.commit()
-
+     c.execute(f"INSERT INTO username_and_password VALUES (?, ?, ?)", (body['username'], hashed_password, body['admin']))
+     conn.commit()
+     conn.close()
+     # query = text(query_string)
+     # session_mssql.execute(query)
+     # session_mssql.commit()
+     print('success')
      return {'username': body['username'], 'password': body['password']}
-     
+
 @app.post('/login')
 def login(auth_details: AuthDetails):
 
-    query_string = "SELECT username, password, admin FROM [AdventureWorks2019].[Person].[UsernameAndPassword]"
-    query = text(query_string)
-    results = session.execute(query).fetchall()
+#     query_string = "SELECT username, password, admin FROM [AdventureWorks2019].[Person].[UsernameAndPassword]"
+#     query = text(query_string)
+#     results = session_mssql.execute(query).fetchall()
+     
+    conn = sqlite3.connect('./master.db')
+    c = conn.cursor()
+    c.execute("""SELECT * FROM username_and_password""")
+    results = c.fetchall()
+    print(results)
     found_user = None
     admin = False
+    print('test')
     for user in results:
          
          print(user)
-         if user[0] == auth_details.username:
-              found_user = {'username' : user[0], 'hashed_password' : user[1]}
-              admin = user[2]
+         if user[1] == auth_details.username:
+              found_user = {'username' : user[1], 'hashed_password' : user[2]}
+              admin = user[3]
               print('found_user: ')
               print(found_user)
               break
-     
+    print('test2')
+    print(found_user['hashed_password'])
     print(auth_handler.verify_password(auth_details.password, found_user['hashed_password']))
-    # We need to GET the hashed password from the SQL db, and pass it as second parameter below where 'found_user' is:
+    print('test3')
+   
     if (found_user is None) or (not auth_handler.verify_password(auth_details.password, found_user['hashed_password'])):
           print('debug test')
           raise HTTPException(status_code = 401, detail = 'Invalid username and / or password')
@@ -86,40 +110,45 @@ def login(auth_details: AuthDetails):
 def protected(username = Depends(auth_handler.auth_wrapper)):
      token = auth_handler.encode_token(username)
      return { 'token': token }
-     
+
 @app.get("/initialFetch")
 async def initial_fetch():
-    # Firstly we need to create the subCat + subCat names object
-    query_string = "SELECT [ProductSubcategoryID], [Name] FROM [AdventureWorks2019].[Production].[ProductSubCategory]"
-    query = text(query_string)
-    results = session.execute(query).fetchall()
+
+    print('test initial fetch')
+    conn = sqlite3.connect('./master.db')
+    c = conn.cursor()
+    c.execute("""SELECT product_subcategory_id, name FROM product_subcategory_id""")
+    results = c.fetchall()
+    print('test test test')
     
     categories = {}
     for row in results:
-         categories[row[0]] = row[1]
+         categories[int(row[0])] = row[1]
     
-
     query_string = "SELECT [name], [ListPrice], [Size], [ProductSubcategoryID], [color], [productID] FROM [AdventureWorks2019].[Production].[Product]"
     query = text(query_string)
-    results = session.execute(query).fetchall()
-    # print(results)
+    results = session_mssql.execute(query).fetchall()
+#     
     array = []
-
-    # We have a dict that has 'key=subCatInt / val = "name"
-    # We have the subCatInt in results, how can we map?
+    
 
     for row in results:
         dictionary = {}
         dictionary['name'] = row[0]
         dictionary['listPrice'] = row[1]
         dictionary['size'] = row[2]
-        dictionary['ProductSubcategoryID'] = row[3]
+        if row[3]:   
+             dictionary['ProductSubcategoryID'] = float(row[3])
+        else:
+             dictionary['ProductSubcategoryID'] = row[3]
         dictionary['color'] = row[4]
         dictionary['productId'] = row[5]
+        
+    
         if row[1] is None or row[1] == 0:
              dictionary['listPrice'] = 10.00
         if row[3] is not None:
-            dictionary['ProductSubcategoryName'] = categories[row[3]]
+             dictionary['ProductSubcategoryName'] = categories[row[3]]
         if row[3] in (1, 2, 3):
              dictionary['ProductCategoryID'] = 1
              dictionary['ProductCategoryName'] = 'Bikes'
@@ -133,41 +162,42 @@ async def initial_fetch():
              dictionary['ProductCategoryID'] = 4
              dictionary['ProductCategoryName'] = 'Accessories'
         if row[3] == None:
-            dictionary['ProductCategoryID'] = 5
-            dictionary['ProductCategoryName'] = 'Misc'
-            dictionary['ProductSubcategoryName'] = 'Hardware'
-        
+             dictionary['ProductCategoryID'] = 5
+             dictionary['ProductCategoryName'] = 'Misc'
+             dictionary['ProductSubcategoryName'] = 'Hardware'
         
         array.append(dictionary)
     
+    conn.commit()
+    conn.close()
     return array
-
 
 @app.put("/editItem")
 async def edit_item(body: dict):
      try:
           parameters = {
-               'x': body.get('name', None),
-               'y': body.get('listPrice', None),
-               "z": body.get('size', None),
-               "a": body.get('color', None),
-               "b": body.get('productId')
+               'name': body.get('name', None),
+               'list_price': body.get('listPrice', None),
+               "size": body.get('size', None),
+               "color": body.get('color', None),
+               "product_id": body.get('productId')
           }
 
           for key, value in parameters.items():
                if value == 'None':
                     parameters[key] = None
-
-          query_string_raw = f"UPDATE [AdventureWorks2019].[Production].[Product] SET [name] = :x , [listPrice] = :y , [size] = :z , [color] = :a WHERE [productId] = :b"
-          query_string = text(query_string_raw)
-          session.execute(query_string, parameters)
-          session.commit()
+          conn = sqlite3.connect('./master.db')
+          c = conn.cursor()
+          c.execute("""
+                    UPDATE product
+                    SET name = ?, list_price = ?, size = ?, color = ?
+                    WHERE product_id = ?
+                    """, (parameters['name'], parameters['list_price'], parameters['size'], parameters['color'], parameters['product_id']))
           return
      except Exception as e:
           print(str(e))
           return HTTPException(status_code=500, detail="Update failed")
      finally:
-          session.close()
+          conn.commit()
+          conn.close()
      
-     
-  
