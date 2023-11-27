f690254dbd03cef92732be6d51f7c5207e000647
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2023-11-26 18:17:59 -0600

    fixed type error on initial fetch

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy import create_engine, text
-
-from sqlalchemy.orm import sessionmaker
 from auth import AuthHandler
 from schemas import AuthDetails
 import sqlite3
-
-engine_mssql = create_engine("mssql+pyodbc://sa:changeme@DESKTOP-RIAC343\\SQLEXPRESS/AdventureWorks2019?driver=ODBC+Driver+17+for+SQL+Server")
-engine_sqlite = create_engine("sqlite:///./.venv/server/master.db")
-
-Session_mssql = sessionmaker(bind = engine_mssql)
-session_mssql = Session_mssql()
-
-Session_sqlite = sessionmaker(bind = engine_sqlite)
-session_sqlite = Session_sqlite()
 
 app = FastAPI()
 auth_handler = AuthHandler()
@@ -25,6 +12,7 @@ origins = [
     'http://localhost:8000',
     'http://localhost:8001',
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:5175'
 ]
 
@@ -38,11 +26,7 @@ app.add_middleware(
 
 @app.post("/createusernameandpassword", status_code = 201)
 def register(body: dict):
-     print('test test test')
-     # Fetch / GET table 'users', and append the col 'usernames' in the list:
-     # query_string = "SELECT username from [AdventureWorks2019].[Person].[UsernameAndPassword]"
-     # query = text(query_string)
-     # results = session_mssql.execute(query).fetchall()
+     
      users = []
      conn = sqlite3.connect('./master.db')
      c = conn.cursor()
@@ -55,34 +39,25 @@ def register(body: dict):
      if any(x == body['username'] for x in users):
           raise HTTPException(status_code = 400, detail = 'Username is taken')
      
-     # If username does not already exist, create a hashed password
      hashed_password = auth_handler.get_password_hash(body['password'])
      c.execute(f"INSERT INTO username_and_password VALUES (?, ?, ?)", (body['username'], hashed_password, body['admin']))
      conn.commit()
      conn.close()
-     # query = text(query_string)
-     # session_mssql.execute(query)
-     # session_mssql.commit()
-     print('success')
+     
+     print('successful account creation')
      return {'username': body['username'], 'password': body['password']}
 
 @app.post('/login')
 def login(auth_details: AuthDetails):
-
-#     query_string = "SELECT username, password, admin FROM [AdventureWorks2019].[Person].[UsernameAndPassword]"
-#     query = text(query_string)
-#     results = session_mssql.execute(query).fetchall()
-     
     conn = sqlite3.connect('./master.db')
     c = conn.cursor()
     c.execute("""SELECT * FROM username_and_password""")
     results = c.fetchall()
-    print(results)
+    
     found_user = None
     admin = False
-    print('test')
+    
     for user in results:
-         
          print(user)
          if user[1] == auth_details.username:
               found_user = {'username' : user[1], 'hashed_password' : user[2]}
@@ -90,20 +65,18 @@ def login(auth_details: AuthDetails):
               print('found_user: ')
               print(found_user)
               break
-    print('test2')
+    
     print(found_user['hashed_password'])
     print(auth_handler.verify_password(auth_details.password, found_user['hashed_password']))
-    print('test3')
-   
+    
     if (found_user is None) or (not auth_handler.verify_password(auth_details.password, found_user['hashed_password'])):
-          print('debug test')
+          
           raise HTTPException(status_code = 401, detail = 'Invalid username and / or password')
     token = auth_handler.encode_token(found_user['username'])
     if admin == 'False':
          admin = False
     else:
          admin = True
-    
     return { 'token': token, 'admin': admin }
 
 @app.get('/istokenvalid')
@@ -113,52 +86,44 @@ def protected(username = Depends(auth_handler.auth_wrapper)):
 
 @app.get("/initialFetch")
 async def initial_fetch():
-
-    print('test initial fetch')
     conn = sqlite3.connect('./master.db')
     c = conn.cursor()
     c.execute("""SELECT product_subcategory_id, name FROM product_subcategory_id""")
     results = c.fetchall()
-    print('test test test')
     
     categories = {}
     for row in results:
-         categories[int(row[0])] = row[1]
-    
-    query_string = "SELECT [name], [ListPrice], [Size], [ProductSubcategoryID], [color], [productID] FROM [AdventureWorks2019].[Production].[Product]"
-    query = text(query_string)
-    results = session_mssql.execute(query).fetchall()
-#     
+         categories[row[0]] = row[1]
+     
+    c.execute("""SELECT name, list_price, size, product_subcategory_id, color, product_id FROM product""")
+    results = c.fetchall()
     array = []
     
-
     for row in results:
         dictionary = {}
         dictionary['name'] = row[0]
         dictionary['listPrice'] = row[1]
         dictionary['size'] = row[2]
         if row[3]:   
-             dictionary['ProductSubcategoryID'] = float(row[3])
-        else:
              dictionary['ProductSubcategoryID'] = row[3]
         dictionary['color'] = row[4]
         dictionary['productId'] = row[5]
-        
-    
+
         if row[1] is None or row[1] == 0:
              dictionary['listPrice'] = 10.00
         if row[3] is not None:
-             dictionary['ProductSubcategoryName'] = categories[row[3]]
-        if row[3] in (1, 2, 3):
-             dictionary['ProductCategoryID'] = 1
-             dictionary['ProductCategoryName'] = 'Bikes'
-        if row[3] is not None and 4 <= row[3] <= 17:
+             dictionary['ProductSubcategoryName'] = categories[int(row[3])]
+        if row[3]:
+             if int(row[3]) in (1, 2, 3):
+                dictionary['ProductCategoryID'] = 1
+                dictionary['ProductCategoryName'] = 'Bikes'
+        if row[3] is not None and 4 <= int(row[3]) <= 17:
              dictionary['ProductCategoryID'] = 2
              dictionary['ProductCategoryName'] = 'Components'
-        if row[3] is not None and 18 <= row[3] <= 25:
+        if row[3] is not None and 18 <= int(row[3]) <= 25:
              dictionary['ProductCategoryID'] = 3
              dictionary['ProductCategoryName'] = 'Clothing'
-        if row[3] is not None and 26 <= row[3] <= 37:
+        if row[3] is not None and 26 <= int(row[3]) <= 37:
              dictionary['ProductCategoryID'] = 4
              dictionary['ProductCategoryName'] = 'Accessories'
         if row[3] == None:
